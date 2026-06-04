ee98f82e021a93b0c434e595fd132b4c6e8cbdd4
Yashraj-singh785 <yashrajsingh7851@gmail.com>
2026-06-04 12:53:23 +0530

    all the requirements successfully installed

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup , find_packages
+
+setup(
+    name = "src",
+    version="0.0.1",
+    author= "Yashraj singh",
+    author_email = "yashrajsingh7851@gmail.com",
+    packages= find_packages()
+)
